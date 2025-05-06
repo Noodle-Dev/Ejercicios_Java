a82803da50c8b0c5de22a48749acebddd7e9ece9
Noodle-Dev <contacto.noodlehairstudios@gmail.com>
2025-05-06 11:43:54 -0600

    ya alv todo

--- Ejercicios_Java/src/Ejercicio_12.java
+++ Ejercicios_Java/src/Ejercicio_12.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Ejercicio_12 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int[] edades = new int[100];
         int contador = 0;
-        int suma = 0;
         
-        System.out.println("Ingrese las edades (número negativo para terminar):");
-        
+        System.out.println("Ingrese las edades (ingrese un número negativo para terminar):");
+
         while (true) {
             System.out.print("Edad " + (contador + 1) + ": ");
             int edad = scanner.nextInt();
@@ -18,17 +18,21 @@ public class Ejercicio_12 {
             }
             
             edades[contador] = edad;
-            suma += edad;
             contador++;
         }
         
-        if (contador > 0) {
-            double promedio = (double) suma / contador;
-            System.out.println("El promedio de edades es: " + promedio);
-        } else {
+        if (contador == 0) {
             System.out.println("No se ingresaron edades válidas.");
+        } else {
+            int suma = 0;
+            for (int i = 0; i < contador; i++) {
+                suma += edades[i];
+            }
+            
+            double promedio = (double) suma / contador;
+            System.out.println("\nPromedio de edades: " + promedio);
         }
+        
+        scanner.close();
     }
 }
-
-//PENDIENTE
--- Ejercicios_Java/src/Ejercicio_14.java
+++ Ejercicios_Java/src/Ejercicio_14.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Ejercicio_14 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese una cadena de texto:");
+        String texto = scanner.nextLine().toLowerCase(); 
+
+        int[] frecuencias = new int[26];
+
+        for (int i = 0; i < texto.length(); i++) {
+            char caracter = texto.charAt(i);
+            if (caracter >= 'a' && caracter <= 'z') {
+                frecuencias[caracter - 'a']++;
+            }
+        }
+        System.out.println("\nHistograma de frecuencias de letras:");
+        for (int i = 0; i < frecuencias.length; i++) {
+            if (frecuencias[i] > 0) {
+                char letra = (char) ('a' + i);
+                System.out.print(letra + ": ");
+                for (int j = 0; j < frecuencias[i]; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+        
+        scanner.close();
     }
 }
